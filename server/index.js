3423baf6a4dda0ee873c897f6a8e07ea62e60dcb
MohanChaturvedhi <120575027+MohanChaturvedhi@users.noreply.github.com>
2023-10-18 23:32:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chatapp-g7b9.onrender.com",
     methods: ["GET", "POST"],
   },
 });
